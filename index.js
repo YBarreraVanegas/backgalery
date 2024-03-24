06fcfe397bece37b4a07273f06922cb93c97dcda
YBarreraVanegas <yojansmithbarrera@gmail.com>
2024-03-24 12:04:25 -0500

    update 1.0

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import app from './src/app.js';
 import { config } from 'dotenv';
-
 config();
-const PORT = process.env.URL || 3000;
+const PORT = process.env.URL || 4000;
 
 // Iniciar el servidor y escuchar en el puerto especificado
 app.listen(PORT, () => {
--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import cors from 'cors';
 import router from './routes/index.rotutes.js';
 const app = express();
 
-app.use(router);
 app.use(cors());
+app.use(router);
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
