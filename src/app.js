445f7b08acf825b439de3289a2acd1accdb45444
YBarreraVanegas <yojansmithbarrera@gmail.com>
2024-03-25 18:52:07 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/index.rotutes.js';
 const app = express();
-
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200, // Algunos navegadores antiguos (como IE11) pueden necesitar esto para que funcione el CORS
+};
+app.use(cors(corsOptions));
 app.use(router);
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
