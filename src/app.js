fc82a0a18466815b30ed4df383aa72e3cabdab69
YBarreraVanegas <yojansmithbarrera@gmail.com>
2024-03-28 23:23:34 -0500

    s

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes/index.rotutes.js';
-import { config } from 'dotenv';
 const app = express();
-config();
 const corsOptions = {
-    origin: 'https://fotofascinante.onrender.com' && process.env.URL_DEV,
-    optionsSuccessStatus: 200, // Algunos navegadores antiguos (como IE11) pueden necesitar esto para que funcione el CORS
 };
 app.use(cors(corsOptions));
 app.use(router);
